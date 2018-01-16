41a489c310dd20483de6d4942f4dc0818391af70
Paradise Kelechi <paradisekelechi@gmail.com>
2018-01-16 23:19:54 +0100

    task(application): create and initialize application
    - start hellobooks spring project
    - add postgresql dependency to pom file

---
+++ src/main/java/com/kelechi/andela/hellobooks/HellobooksApplication.java
@@ -0,0 +1,12 @@
+package com.kelechi.andela.hellobooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobooksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kelechi/andela/hellobooks/HellobooksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kelechi.andela.hellobooks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellobooksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
